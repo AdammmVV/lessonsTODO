bedb42bbdbc30736e5bbc12fce9d18f94af25d7e
Adammm <adammm.web@gmail.com>
2023-01-22 23:54:54 +0300

    add function addTask and showClickHandler

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {SupperButton} from "./components/SupperButton";
+import {SuperInput} from "./components/SuperInput";
+import {v1} from "uuid";
+
+type ServType = {
+    completed: boolean
+    id: number
+    title: string
+    userId: number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState<ServType[]>([])
+    const [show, seShow] = useState(false)
+    const[newTitle, setNewTitle] = useState('')
+
+    const myFetch = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setState(json))
+        console.log(state)
+    }
+
+    useEffect(()=> myFetch,[])
+
+    const showClickHandler = () => {
+        seShow(!show)
+    }
+
+    const AddTask = () => {
+        setState([{userId: +(v1()), id: +(v1()), title: newTitle, completed: false}, ...state])
+        setNewTitle('')
+    }
+
+    console.log(newTitle)
+
+    return (
+        <div className="App">
+            <SupperButton name={'Get Data'} callBack={myFetch}/>
+            <SupperButton name={'Render'} callBack={showClickHandler}/>
+            <SuperInput setTitle={setNewTitle} newTitle={newTitle}/>
+            <SupperButton name={'Add'} callBack={AddTask} />
+            <ul>
+                {show && state.map(el => <li key={el.id}><div>{el.title} <input type='checkbox' checked={el.completed}/></div></li>)}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
